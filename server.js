304a179cdc63e16e8efe4ff2aad31886f0502f7e
oscar <oscar.mendez@informatech.cr>
2013-08-14 09:53:51 -0600

    Creating server

--- server.js
+++ server.js
@@ -1,37 +1,46 @@
 ﻿// Node.js entry point
 
-var aws = require("./libs/aws");
+var http = require('http')
 
-var accessKeyId = "AKIAISWN4YIRSTNPXR3A";
+var port = process.env.PORT || 1337;
 
-var secretAccessKey = "oBgzBizNyBzH39oML3bEF9Kofgl7Nnl0fDE80kOz";
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Azure');
+}).listen(port)
 
-ses = aws.createSESClient(accessKeyId, secretAccessKey);
+//var aws = require("./libs/aws");
 
-ses.call("GetSendQuota", {}, function (err, result) {
-    console.log(JSON.stringify(result));
-});
+//var accessKeyId = "AKIAISWN4YIRSTNPXR3A";
 
-ses.call("GetSendStatistics", {}, function (err, result) {
-    console.log(JSON.stringify(result));
-});
+//var secretAccessKey = "oBgzBizNyBzH39oML3bEF9Kofgl7Nnl0fDE80kOz";
 
-ses.call("ListVerifiedEmailAddresses", {}, function (err, result) {
-    console.log(JSON.stringify(result));
-});
+//ses = aws.createSESClient(accessKeyId, secretAccessKey);
 
-var recipient_address = 'oscar@informatech.cr';
-var sender_address = 'oscar.mendez@informatech.cr';
-var send_args = {
-    'Destination.ToAddresses.member.1': recipient_address,
-    'Message.Body.Text.Charset': 'UTF-8',
-    'Message.Body.Text.Data': 'Hello text!',
-    'Message.Body.Html.Charset': 'UTF-8',
-    'Message.Body.Html.Data': '<b>Hello body!</b>',
-    'Message.Subject.Charset': 'UTF-8',
-    'Message.Subject.Data': 'Test node.js and SES',
-    'Source': sender_address
-};
-ses.call('SendEmail', send_args, function (err, result) {
-    console.log(result);
-});
+//ses.call("GetSendQuota", {}, function (err, result) {
+//    console.log(JSON.stringify(result));
+//});
+
+//ses.call("GetSendStatistics", {}, function (err, result) {
+//    console.log(JSON.stringify(result));
+//});
+
+//ses.call("ListVerifiedEmailAddresses", {}, function (err, result) {
+//    console.log(JSON.stringify(result));
+//});
+
+//var recipient_address = 'oscar@informatech.cr';
+//var sender_address = 'oscar.mendez@informatech.cr';
+//var send_args = {
+//    'Destination.ToAddresses.member.1': recipient_address,
+//    'Message.Body.Text.Charset': 'UTF-8',
+//    'Message.Body.Text.Data': 'Hello text!',
+//    'Message.Body.Html.Charset': 'UTF-8',
+//    'Message.Body.Html.Data': '<b>Hello body!</b>',
+//    'Message.Subject.Charset': 'UTF-8',
+//    'Message.Subject.Data': 'Test node.js and SES',
+//    'Source': sender_address
+//};
+//ses.call('SendEmail', send_args, function (err, result) {
+//    console.log(result);
+//});
